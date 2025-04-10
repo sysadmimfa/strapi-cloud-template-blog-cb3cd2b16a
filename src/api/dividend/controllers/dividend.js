e834a5c392d4b5838725964c05051a7b97e29025
kishorlangoor <kishor.kumar@langoor.com>
2025-04-10 12:07:16 +0530

    dividend added

---
+++ src/api/dividend/controllers/dividend.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dividend controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::dividend.dividend');
